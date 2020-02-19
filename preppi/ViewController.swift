1d6f49131f1fe50ffbf900532ef168658bb5790c
Mathusan Selvarajah <mathusans52@gmail.com>
2020-02-18 23:16:48 -0500

    initialize project

---
+++ preppi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  preppi
+//
+//  Created by Mathusan Selvarajah on 18/2/20.
+//  Copyright © 2020 Mathusan Selvarajah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
